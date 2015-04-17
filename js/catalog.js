50dbeea33f1a11255e157bb798521f5131a3b86f
Julian Egbert <jjegbe01@louisville.edu>
2015-04-16 20:11:29 -0400

    Added in new project using JS and Backbone

---
+++ js/catalog.js
@@ -0,0 +1,16 @@
+var CATALOG = (function () {
+
+	var catalog = {
+		songs : [
+			{ 
+				'id' : 1,
+				'title' : 'LL1',
+				'basePath' : './music/song1', 
+				'formats' : [ 'ogg' ]
+			},
+			
+		]
+	};
+
+	return catalog;
+})();
